48176b75f80735df57d326bf766ff41f37cade59
Basekill <stevenshine@hotmail.co.uk>
2021-07-16 22:50:04 +0100

    Added first test from lecture notes to DNA sequences

---
+++ 1_DNA_Sequences.py
@@ -0,0 +1,8 @@
+
+def longest_subsequence_length(string1,string2):
+    pass
+
+def test_longest_subsequence_length():
+    assert longest_subsequence_length("CCGTCAGTCGCG","TGTTTCGGAATGCAA") == 3
+
+
