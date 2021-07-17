6e63b7a492f468873394de44da6c14e0fcd2ef41
Basekill <stevenshine@hotmail.co.uk>
2021-07-17 08:33:16 +0100

    Tried to create a function that gets all possible subsets but does not work yet

--- 1_DNA_Sequences.py
+++ 1_DNA_Sequences.py
@@ -1,8 +1,17 @@
 
+
+
 def longest_subsequence_length(string1,string2):
-    pass
+    sub1 = []
+    for i in range(len(string1)):
+        for j in range(i+1,len(string1)+1):
+            sub1.append(string1[i:j])
+    print(sub1)
+
 
 def test_longest_subsequence_length():
     assert longest_subsequence_length("CCGTCAGTCGCG","TGTTTCGGAATGCAA") == 3
+
+longest_subsequence_length("ABC","ABC")
 
 
